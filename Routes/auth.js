565fab5a72135e4d50bd38e9d67598291b5510a2
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-20 21:22:10 +0500

    feat: Waiting for mongoose to save, before sending response

--- Routes/auth.js
+++ Routes/auth.js
@@ -12,13 +12,13 @@ router.post('/add-user', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 8 characters long').isLength({ min: 8 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    const user = User(req.body);
-    user.save();
+    let user = new User(req.body);
+    user = await user.save();
     return res.json(user);
 });
 
