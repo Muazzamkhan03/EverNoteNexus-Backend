e3565b45b539d974e3985d161d4efb1e55ce1989
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-20 23:16:02 +0500

    feat: Improved the check for existing user and handled errors properly

--- Routes/auth.js
+++ Routes/auth.js
@@ -12,27 +12,30 @@ router.post('/add-user', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 8 characters long').isLength({ min: 8 })
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+],
+    async (req, res) => {
+        // Checks for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    try{
-        let user = new User(req.body);
-        user = await user.save();
-        return res.json(user);
-    }
-    catch(error){
-        console.log("Error: ", error);
-        res.json({
-            errors:[
-                {
-                    error: "Email already in use"
-                }
-            ]
-        });
-    }
-});
+        try {
+            // Checks for existing emails  
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ errors: [{ error: "Email already in use" }] });
+            }
+
+            // Creates a new user
+            user = new User(req.body);
+            user = await user.save();
+            return res.json(user);
+        }
+        catch (error) {
+            console.log("Error: ", error);
+            res.status(500).send("Internal server error occured");
+        }
+    });
 
 module.exports = router;
