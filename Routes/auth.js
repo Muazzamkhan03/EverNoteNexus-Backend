bf6421cef98164e4246985a56b1f7430ac24a352
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-20 20:45:51 +0500

    feat: Added an endpoint for registering a user

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const User = require('../Models/User');
+
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.json('Hit api endpoint for auth');
+})
+
+router.post('/add-user', (req,res)=>{
+    const user = User(req.body);
+    user.save();
+    res.json(req.body);
 })
 
 module.exports = router;
