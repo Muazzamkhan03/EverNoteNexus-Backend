d6f83faa7ccdd39ad4b39ccee9666f3c9ae12787
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-20 19:43:09 +0500

    feat: Made the models for user and notes

---
+++ Models/Note.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('notes',NoteSchema);
