f740201388eecbfb79aa5b18f07faea2b34ed810
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-20 21:13:24 +0500

    feat: Added validation using express validator

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const User = require('../Models/User');
-const { query, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.json('Hit api endpoint for auth');
 })
 
-router.post('/add-user', (req,res)=>{
+router.post('/add-user', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 8 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const user = User(req.body);
     user.save();
-    res.json(req.body);
-})
+    // res.json(req.body);
+    return res.send(`Hello, ${req.body.name}!`);
+});
 
 module.exports = router;
