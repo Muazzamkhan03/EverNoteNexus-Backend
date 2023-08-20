cf9bc11c600c50934330d20329e67277f2d3e71f
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-20 18:41:16 +0500

    feat: Started a server listening on port 3000

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const connect = require('./connection');
+const connectDb = require('./connection');
+const express = require('express');
 
-connect();
+connectDb();
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+})
