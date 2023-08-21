021ab2560bc3cf12802457a5f5c7a004e9e14868
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-21 17:03:46 +0500

    feat: Made an enndpoint for fetching all the notes

--- Routes/note.js
+++ Routes/note.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// ROUTE 1: Default endpoint. No login required
 router.get('/',(req,res)=>{
     res.json('Hit api endpoint for notes');
 })
+
+// ROUTE 2:  Endpoint for fetching all notes using GET /api/notes/fetch-notes. Login required
+router.get('/fetch-notes', (req,res)=>{
+
+});
 
 module.exports = router;
