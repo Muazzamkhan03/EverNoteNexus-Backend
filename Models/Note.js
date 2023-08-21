3ee07ab1bc58c6e85216bed3c910b021e7069d06
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-21 17:17:54 +0500

    feat: Added userId as foreign key in the notes

--- Models/Note.js
+++ Models/Note.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId
+    },
     title: {
         type: String,
         required: true
