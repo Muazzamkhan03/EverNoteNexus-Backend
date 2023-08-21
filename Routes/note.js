ab34eba11c9d46761ebe918b1e4e65512ac6f02d
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-22 00:41:42 +0500

    feat: Added an end point for deleting a note

--- Routes/note.js
+++ Routes/note.js
@@ -6,14 +6,14 @@ const fetchUser = require('../MiddleWare/fetchUser');
 const router = express.Router();
 
 // ROUTE 1: Default endpoint. No login required
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.json('Hit api endpoint for notes');
 })
 
 // ROUTE 2:  Endpoint for fetching all notes using GET /api/notes/fetch-notes. Login required
-router.get('/fetch-notes', fetchUser, async (req,res)=>{
-    try{
-        const notes = await Note.find({user: req.user.id});
+router.get('/fetch-notes', fetchUser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
         res.json(notes);
     } catch (error) {
         console.log("Error: ", error);
@@ -25,8 +25,8 @@ router.get('/fetch-notes', fetchUser, async (req,res)=>{
 router.post('/add-note', fetchUser, [
     body('title', 'Title cannot be blank').exists(),
     body('description', 'Description cannot be blank').exists()
-], 
-async (req, res) => {
+],
+    async (req, res) => {
         // Checks for validation errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -34,7 +34,7 @@ async (req, res) => {
         }
 
         // Destructuring the request body to get individual elements
-        const {title, description, tag} = req.body;
+        const { title, description, tag } = req.body;
 
         try {
             // Creating a new note
@@ -55,30 +55,56 @@ async (req, res) => {
 
     });
 
-// ROUTE 4:  Endpoint for updating an existing note using PUT /api/notes/update-note. Login required
-router.put('/update-note/:id',fetchUser, async (req, res)=>{
-    //Creating a new note object, that would update the existing note
-    const newNote = {};
-    if(req.body.title){newNote.title = req.body.title}
-    if(req.body.description){newNote.description = req.body.description}
-    if(req.body.tag){newNote.tag = req.body.tag}
+// ROUTE 4:  Endpoint for updating an existing note using PUT /api/notes/update-note/:id. Login required
+router.put('/update-note/:id', fetchUser, async (req, res) => {
+    try {
+        //Creating a new note object, that would update the existing note
+        const newNote = {};
+        if (req.body.title) { newNote.title = req.body.title }
+        if (req.body.description) { newNote.description = req.body.description }
+        if (req.body.tag) { newNote.tag = req.body.tag }
 
-    // Checking if the note with the passed id exists 
-    let note = await Note.findById(req.params.id);
-    if(!note){
-        return res.status(404).send("Not Found");
-    }
-    
-    // Checking if the user id in the note, matches with the user id passed extracted from the fetchUser middleware 
-    if(note.user.toString() !== req.user.id){
-        return res.status(401).send("Not Allowed");
+        // Checking if the note with the passed id exists 
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
 
-    }
+        // Checking if the user id in the note, matches with the user id extracted from the fetchUser middleware 
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
 
-    // Updating the note
-    try{
-        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+        // Updating the note
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
         res.json(note);
+
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).send("Internal server error occured");
+    }
+
+});
+
+// ROUTE 5:  Endpoint for deleting a note using DELETE /api/notes/delete-note/:id. Login required
+router.delete('/delete-note/:id', fetchUser, async (req, res) => {
+    try {
+        // Checking if the note with the passed id exists 
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
+
+        // Checking if the user id in the note, matches with the user id extracted from the fetchUser middleware 
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        // Updating the note
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({"Success": "Note has been deleted", note: note});
+
     } catch (error) {
         console.log("Error: ", error);
         res.status(500).send("Internal server error occured");
